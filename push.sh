1c35a6d7eb95e6fbbe19ec5e76995ecd7d592fad
cyt <ytchen5@163.com>
2021-06-14 10:32:07 +0800

    chenyantao@cyt

--- push.sh
+++ push.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-git status &>/dev/null
+LOG_FILE=./push.log
+echo "###########`date +%F-%H:%M:%m`#############">>$LOG_FILE
+git status >>$LOG_FILE
 sleep 2
 git add .
 sleep 2
-git commit -m "chenyantao@cyt" &>/dev/null
+git commit -m "chenyantao@cyt" >>$LOG_FILE
 sleep 2
-git push origin master &>/dev/null
+git push origin master >>$LOG_FILE
 sleep 10
 echo "push ok"
